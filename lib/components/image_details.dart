1f8f8008a6758f983c7598499c9fa0bf6686f652
HovaUseless <88477974+HovaUseles@users.noreply.github.com>
2023-08-22 16:16:38 +0200

    Added delete and fullscreen image

---
+++ lib/components/image_details.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:image_gallery/models/image.dart';
+
+class ImageDetails extends StatelessWidget {
+  final ImageItem _imageItem;
+
+  const ImageDetails(this._imageItem, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(25),
+      // decoration: BoxDecoration(
+      //   color: Theme.of(context).colorScheme.background
+      // ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(
+                  builder: (context) {
+                    return Dialog.fullscreen(
+                      backgroundColor: Colors.black,
+                      child: Image.memory(_imageItem.bytes),
+                    );
+                  }
+                  ),
+                  );
+            },
+            child: Image.memory(_imageItem.bytes),
+          ),
+          Text(
+            "Name: ${_imageItem.name}",
+            textScaleFactor: 2,
+          ),
+          Text(
+            "Size: ${_imageItem.size}",
+            textScaleFactor: 2,
+          ),
+        ],
+      ),
+    );
+  }
+}
